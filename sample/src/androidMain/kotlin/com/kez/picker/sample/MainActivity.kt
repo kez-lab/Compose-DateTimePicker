c49e2a320c17587f6c8b0f2b44f82af388154133
Kez <kejprogramer0909@gmail.com>
2026-05-21 19:01:00 +0900

    docs: use shared sample app theme at entry

--- sample/src/androidMain/kotlin/com/kez/picker/sample/MainActivity.kt
+++ sample/src/androidMain/kotlin/com/kez/picker/sample/MainActivity.kt
@@ -4,14 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +11,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            MaterialTheme {
-                App()
-            }
+            App()
         }
     }
-} 
+}
